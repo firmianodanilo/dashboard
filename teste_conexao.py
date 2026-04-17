c26a280885a0e08a1fd7d5f7fcea3b208ab9be64
firmianodanilo <danilo.firmiano88@gmail.com>
2026-04-17 06:42:11 -0300

    Update teste_conexao.py

--- teste_conexao.py
+++ teste_conexao.py
@@ -4,7 +4,7 @@ con = fdb.connect(
     dsn='c:/integraw/integraw.fdb',
     user='SYSDBA',
     password='masterkey',
-       
+    fb_library_name='/usr/lib/x86_64-linux-gnu/libfbclient.so'   
 )
 
 cur = con.cursor()
