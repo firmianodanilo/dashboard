df49193884e7de4bf4a4160179c61483687a40b3
firmianodanilo <danilo.firmiano88@gmail.com>
2026-04-17 06:35:06 -0300

    Update teste_conexao.py

--- teste_conexao.py
+++ teste_conexao.py
@@ -4,8 +4,7 @@ con = fdb.connect(
     dsn='c:/integraw/integraw.fdb',
     user='SYSDBA',
     password='masterkey',
-     fb_library_name=r"C:\Program Files\Firebird\Firebird_2_5\bin\fbclient.dll"
-   
+       
 )
 
 cur = con.cursor()
